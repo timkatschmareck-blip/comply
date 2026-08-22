28a42ac07c48f4587c825caf5acd7029074b559d
Tim Katschmareck <tim@comply.app>
2026-08-22 19:03:01 +0200

    fix: restore AppProvider

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,6 +1,6 @@
 import { Outlet, useLoaderData, useRouteError } from "react-router";
-import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
