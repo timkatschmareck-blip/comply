e70dfb75e8a77d942a29daa6fba926d29e7a38d4
Tim Katschmareck <tim@comply.app>
2026-08-23 15:19:08 +0200

    fix: build error remove db import

--- app/routes/api.export.jsx
+++ app/routes/api.export.jsx
@@ -1,12 +1,13 @@
-import db from "../db.server";
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const shop = url.searchParams.get("shop") || "comply-test-bochum.myshopify.com";
 
-export const loader = async () => {
-  const shop = "comply-test-bochum.myshopify.com";
   const csv = `sep=;
 Material;Masse_kg;Bestellungen;Shop
 Karton;0.2;1;${shop}
 Gesamt;0.2;1;${shop}
 `;
+
   return new Response(csv, {
     headers: {
       "Content-Type": "text/csv; charset=utf-8",
